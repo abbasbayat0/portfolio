4f281447480100bd5b4b928b99af9516f75c2f7e
abbasbayat0 <abbas0bayat@gmail.com>
2026-07-14 11:06:58 +0330

    hero image loading

--- src/components/sections/Main/Hero/Image.tsx
+++ src/components/sections/Main/Hero/Image.tsx
@@ -13,7 +13,7 @@ const Image = () => {
           animate={{ translateX: 0, opacity: 1, scale: 1 }}
           transition={{ duration: 1, delay: 1.2, ease: "backIn" }}
           src={imageUrl}
-          onLoad={() => setImageLoaded(true)}
+          onLoad={() => setImageLoaded(false)}
           alt="abbas bayat true picture"
           className={`relative rounded-xl border-5 border-[#1a2329]/30`}
         />
@@ -23,7 +23,7 @@ const Image = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1, delay: 2.5, ease: "backIn" }}
-          className="lg:py-auto lg:px-auto absolute -top-1/12 -left-4 flex animate-[ex_3s_ease-in-out_infinite] items-center justify-center rounded-xl border border-[#1a2329] bg-[#1a2329]/50 px-1 py-1 backdrop-blur-xs min-[500px]:-top-8 min-[500px]:-left-5 min-[870px]:-top-8 lg:-top-10 lg:-left-8 lg:h-20 lg:w-24"
+          className="lg:py-auto lg:px-auto absolute -top-1/12 -left-4 z-10 flex animate-[ex_3s_ease-in-out_infinite] items-center justify-center rounded-xl border border-[#1a2329] bg-[#1a2329]/50 px-1 py-1 backdrop-blur-xs min-[500px]:-top-8 min-[500px]:-left-5 min-[870px]:-top-8 lg:-top-10 lg:-left-8 lg:h-20 lg:w-24"
         >
           <p className="text-xs font-extralight tracking-wide text-white/60">
             <span className="text-green text-2xl font-bold lg:text-3xl">
@@ -39,7 +39,7 @@ const Image = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1, delay: 2.5, ease: "backIn" }}
-          className="lg:px-auto lg:py-auto absolute -right-4 bottom-0 flex animate-[ex_3s_ease-in-out_infinite] items-center justify-center gap-2 rounded-xl border border-[#1a2329] bg-[#1a2329]/50 px-2 py-1 backdrop-blur-xs lg:-right-8 lg:-bottom-4 lg:h-10 lg:w-40"
+          className="lg:px-auto lg:py-auto absolute -right-4 bottom-0 z-10 flex animate-[ex_3s_ease-in-out_infinite] items-center justify-center gap-2 rounded-xl border border-[#1a2329] bg-[#1a2329]/50 px-2 py-1 backdrop-blur-xs lg:-right-8 lg:-bottom-4 lg:h-10 lg:w-40"
         >
           <div className="h-3 w-3 animate-pulse rounded-full bg-[#01BE4C]" />
           <p className="text-xs text-white/90 lg:text-xs">Available For Work</p>
@@ -51,7 +51,7 @@ const Image = () => {
         initial={{ opacity: 0, scale: 0.95 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 1, delay: 1.8, ease: "backIn" }}
-        className={`shadow-green/30 absolute max-w-113 rounded-xl shadow-[0px_0px_10px_10px] transition-opacity duration-1000 min-[870px]:w-11/12 lg:w-full ${imageLoaded ? "bg-transparent blur-none" : "bg-black/90 blur-sm"}`}
+        className={`shadow-green/30 absolute max-w-113 rounded-xl shadow-[0px_0px_10px_10px] transition duration-1000 min-[870px]:w-11/12 lg:w-full ${imageLoaded ? "bg-black/0 blur-none" : "bg-black blur-sm"}`}
       >
         <img
           src={import.meta.env.BASE_URL + "abbas bayat.png"}
