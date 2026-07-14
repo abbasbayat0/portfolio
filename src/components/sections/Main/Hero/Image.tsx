d46b8cee8c55b76216ac0a2f7fb5ed840c0735f9
abbasbayat0 <abbas0bayat@gmail.com>
2026-07-14 11:50:41 +0330

    resume edited

--- src/components/sections/Main/Hero/Image.tsx
+++ src/components/sections/Main/Hero/Image.tsx
@@ -22,9 +22,11 @@ const Image = () => {
 
         {/* EXP */}
         <motion.div
-        initial={{opacity:0}}
-        animate={{opacity:1}}
-        transition={{duration:1, delay:2, ease:"backIn"}} className="lg:py-auto lg:px-auto absolute -top-1/12 -left-4 z-10 flex animate-[ex_3s_ease-in-out_infinite] items-center justify-center rounded-xl border border-[#1a2329] bg-[#1a2329]/50 px-1 py-1 backdrop-blur-xs min-[500px]:-top-8 min-[500px]:-left-5 min-[870px]:-top-8 lg:-top-10 lg:-left-8 lg:h-20 lg:w-24">
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 2, ease: "backIn" }}
+          className="lg:py-auto lg:px-auto absolute -top-1/12 -left-4 z-10 flex animate-[ex_3s_ease-in-out_infinite] items-center justify-center rounded-xl border border-[#1a2329] bg-[#1a2329]/50 px-1 py-1 backdrop-blur-xs min-[500px]:-top-8 min-[500px]:-left-5 min-[870px]:-top-8 lg:-top-10 lg:-left-8 lg:h-20 lg:w-24"
+        >
           <p className="text-xs font-extralight tracking-wide text-white/60">
             <span className="text-green text-2xl font-bold lg:text-3xl">
               2+
@@ -36,9 +38,11 @@ const Image = () => {
 
         {/* available */}
         <motion.div
-        initial={{opacity:0}}
-        animate={{opacity:1}}
-        transition={{duration:1, delay:2, ease:"backIn"}} className="lg:px-auto lg:py-auto absolute -right-4 bottom-0 z-10 flex animate-[ex_3s_ease-in-out_infinite] items-center justify-center gap-2 rounded-xl border border-[#1a2329] bg-[#1a2329]/50 px-2 py-1 backdrop-blur-xs lg:-right-8 lg:-bottom-4 lg:h-10 lg:w-40">
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 2, ease: "backIn" }}
+          className="lg:px-auto lg:py-auto absolute -right-4 bottom-0 z-10 flex animate-[ex_3s_ease-in-out_infinite] items-center justify-center gap-2 rounded-xl border border-[#1a2329] bg-[#1a2329]/50 px-2 py-1 backdrop-blur-xs lg:-right-8 lg:-bottom-4 lg:h-10 lg:w-40"
+        >
           <div className="h-3 w-3 animate-pulse rounded-full bg-[#01BE4C]" />
           <p className="text-xs text-white/90 lg:text-xs">Available For Work</p>
         </motion.div>
